aec13c5e0f88b4a2595331d7a674710de570988a
hey3 <oh1kohei@gmail.com>
2020-08-13 15:36:52 +0900

    fix lint warning (#17)

--- src/saga/sagas/index.ts
+++ src/saga/sagas/index.ts
@@ -1,5 +1,5 @@
-import { all } from 'redux-saga/effects'
+import { AllEffect, ForkEffect, all } from 'redux-saga/effects'
 
-export const rootSaga = function* root() {
+export const rootSaga = function* root(): Generator<AllEffect<ForkEffect<void>>, void> {
   yield all([])
 }
